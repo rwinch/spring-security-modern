9a4256de7847adac0c2e63450bbc827e0a821405
Rob Winch <rwinch@gopivotal.com>
2014-07-14 09:03:48 -0500

    Acegi

---
+++ src/main/java/sample/security/UserRepositoryUserDetailsService.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2002-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package sample.security;
+
+import java.util.Collection;
+
+import org.acegisecurity.GrantedAuthority;
+import org.acegisecurity.GrantedAuthorityImpl;
+import org.acegisecurity.userdetails.UserDetails;
+import org.acegisecurity.userdetails.UserDetailsService;
+import org.acegisecurity.userdetails.UsernameNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import sample.data.User;
+import sample.data.UserRepository;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Rob Winch
+ *
+ */
+@Service("userDetailsService")
+public class UserRepositoryUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserRepositoryUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+        if(user == null) {
+            throw new UsernameNotFoundException("Could not find user " + username);
+        }
+        return new CustomUserDetails(user);
+    }
+
+    private final static class CustomUserDetails extends User implements UserDetails {
+
+        private CustomUserDetails(User user) {
+            super(user);
+        }
+
+        @Override
+        public GrantedAuthority[] getAuthorities() {
+            return new GrantedAuthority[] { new GrantedAuthorityImpl("ROLE_USER") };
+        }
+
+        @Override
+        public String getUsername() {
+            return getEmail();
+        }
+
+        @Override
+        public boolean isAccountNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isAccountNonLocked() {
+            return true;
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return true;
+        }
+
+        private static final long serialVersionUID = 5639683223516504866L;
+    }
+}
